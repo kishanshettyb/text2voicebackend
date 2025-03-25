b46bf1a18efd1970bd3d3ca1553ec5670d48a2af
Kishan B <kishanqr@gmail.com>
2025-03-25 16:57:37 +0530

    feat: :sparkles: add new schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -382,9 +382,6 @@ export interface ApiTextToVoiceGenerationTextToVoiceGeneration
     draftAndPublish: true;
   };
   attributes: {
-    audio_format: Schema.Attribute.String & Schema.Attribute.Required;
-    audio_url: Schema.Attribute.String & Schema.Attribute.Required;
-    character_count: Schema.Attribute.Integer & Schema.Attribute.Required;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
@@ -395,17 +392,12 @@ export interface ApiTextToVoiceGenerationTextToVoiceGeneration
     > &
       Schema.Attribute.Private;
     publishedAt: Schema.Attribute.DateTime;
-    text: Schema.Attribute.Text & Schema.Attribute.Required;
+    title: Schema.Attribute.Text & Schema.Attribute.Required;
+    uid: Schema.Attribute.UID & Schema.Attribute.Required;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    user: Schema.Attribute.Relation<
-      'manyToOne',
-      'plugin::users-permissions.user'
-    > &
-      Schema.Attribute.Private;
-    voice_name: Schema.Attribute.String & Schema.Attribute.Required;
-    voice_speed: Schema.Attribute.String & Schema.Attribute.Required;
+    voices: Schema.Attribute.Relation<'oneToMany', 'api::voice.voice'>;
   };
 }
 
@@ -432,6 +424,10 @@ export interface ApiVoiceVoice extends Struct.CollectionTypeSchema {
       Schema.Attribute.Private;
     publishedAt: Schema.Attribute.DateTime;
     text: Schema.Attribute.Text & Schema.Attribute.Required;
+    text_to_voice_generation: Schema.Attribute.Relation<
+      'manyToOne',
+      'api::text-to-voice-generation.text-to-voice-generation'
+    >;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
@@ -929,10 +925,6 @@ export interface PluginUsersPermissionsUser
     role: Schema.Attribute.Relation<
       'manyToOne',
       'plugin::users-permissions.role'
-    >;
-    text_to_voice_generations: Schema.Attribute.Relation<
-      'oneToMany',
-      'api::text-to-voice-generation.text-to-voice-generation'
     >;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
